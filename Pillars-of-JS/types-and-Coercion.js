c014507e587db3e13ecfb6a03f49b0b80b0918f1
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2020-02-11 18:19:26 -0500

    created notes on scope and closure

--- Pillars-of-JS/types-and-Coercion.js
+++ Pillars-of-JS/types-and-Coercion.js
@@ -1,7 +1,7 @@
-// Types and Coercion
-// In Javascript variables don't have types, values do 
 /*
--Converting Types
+Types and Coercion
+In Javascript variables don't have types, values do 
+
 -Checking Equality
 */
 
@@ -38,3 +38,42 @@ typeof v; // "object"
 
 v = [1,2,3];
 typeof v; //"object" 
+
+//-------------------------------------------
+// Fundamental Objects
+/* Use the "new" keyword:
+   - Object()
+   - Array()
+   - Function()
+   - Date()
+   - RegExp()
+   - Error()
+*/ // Use "new" Keyword when you want to create object representations
+
+let yesterday = new Date("February 11, 2020");
+yesterday.toUTCString();
+// "Tue, 11 Feb 2020 4:10:00 EST"
+
+/* DONT use "new" keyword :
+   - String()
+   - Number()
+   - Boolean()
+*/
+// skip the "new" Keyword when you are calling a fundamental object to convert a type
+
+let myGPA = String(transcript.gpa)
+// "3.23"
+
+//--------------------------------------------
+// -Converting Types
+
+// Boolean "falsy" values become false when converted into boolean
+/*
+Falsy values:
+"" <- empty string
+0, -0
+null
+NaN
+false
+undefined
+ */
