1c08919d258a1c7bbea8f6b3cd9d1f63058f31ea
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2020-02-11 16:24:35 -0500

    added notes on primitive types and fundamental objects

--- Pillars-of-JS/types-and-Coercion.js
+++ Pillars-of-JS/types-and-Coercion.js
@@ -1,6 +1,40 @@
 // Types and Coercion
+// In Javascript variables don't have types, values do 
 /*
--Primitive Types
 -Converting Types
 -Checking Equality
 */
+
+/*
+-Primitive Types
+  1. undefined
+  2. string
+  3. boolean
+  4. number
+  5. object
+  6. symbol
+  7. ! Null (has some quirks to be aware of)
+*/
+var v;
+typeof v; // "undefined"
+
+v = "1";
+typeof v; // "string"
+
+v = 2;
+typeof v; // "number"
+
+v = true;
+typeof v; // "boolean"
+
+v = {};
+typeof v; // "object"
+
+v = Symbol();
+typeof v; // "symbol"
+
+var v = null;
+typeof v; // "object" 
+
+v = [1,2,3];
+typeof v; //"object" 
